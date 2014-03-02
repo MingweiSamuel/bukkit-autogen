4f50c96e65e7ba438a38ae797428fd560cb55384
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2014-03-01 22:15:13 -0800

    Semi-functional build

--- src/com/lugtech/bukkitautogen/AutogenProcessor.java
+++ src/com/lugtech/bukkitautogen/AutogenProcessor.java
@@ -1,31 +1,48 @@
 package com.lugtech.bukkitautogen;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 
 @SupportedAnnotationTypes("com.lugtech.bukkitautogen.CommandInfo")
 public class AutogenProcessor extends AbstractProcessor {
-
+	
 	@Override
-	public boolean process(Set<? extends TypeElement> elements, RoundEnvironment roundEnv) {
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 		Messager msg = super.processingEnv.getMessager();
+		Types typeUtil = super.processingEnv.getTypeUtils();
+		Elements elementUtil = super.processingEnv.getElementUtils();
 		
-		for (TypeElement element : elements) {
-			if (element.getInterfaces().contains(AutogenCommand.class)) {
-				msg.printMessage(Diagnostic.Kind.WARNING, "Warning here");
-				msg.printMessage(Diagnostic.Kind.WARNING, "Warning here", element);
-				if (element.getAnnotation(CommandInfo.class) == null)
-					msg.printMessage(Diagnostic.Kind.ERROR, "Class must have @CommandInfo annotation", element);
+		Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(CommandInfo.class);
+		
+		for (Element element : elements) {
+			if (element instanceof TypeElement) {
+				TypeElement typeElement = (TypeElement) element;
+				Iterator<? extends TypeMirror> interfaces = typeElement.getInterfaces().iterator();
+				
+				while (true) {
+					if (!interfaces.hasNext()) {
+						msg.printMessage(Diagnostic.Kind.ERROR, "Classes using the @CommandInfo annotation must implement the org.bukkit.command.CommandExecutor interface", element);
+						return true;
+					}
+					if (typeUtil.isSameType(interfaces.next(), elementUtil.getTypeElement("org.bukkit.command.CommandExecutor").asType()))
+						break;
+				}
+				//the usage is valid
+				
 			}
 		}
 		
 		return true;
 	}
-
 }
